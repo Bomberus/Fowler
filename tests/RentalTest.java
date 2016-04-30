512909761efaa9e725164334da87f979b02fe252
Pascal Maximilian Bremer <maximilian@cdsffo.de>
2016-04-30 19:21:47 +0200

    Moved Method amountFor from Customer to Rental, because it was used mostly by this class. Added a test to check functionality

--- tests/RentalTest.java
+++ tests/RentalTest.java
@@ -4,6 +4,46 @@ import static org.junit.Assert.*;
 public class RentalTest {
     @Test
     public void createTest(){
+        Movie movie = new Movie("Marlene Mortler und die Drogenaffäre",Movie.REGULAR);
+        Rental rental = new Rental(movie, 10);
 
+        assertEquals(10,rental.getDaysRented());
+        assertEquals(movie,rental.getMovie());
     }
+
+    @Test
+    public void getMoneyTest(){
+        String title = "XXL dreckiges Geschirr";
+        for (int i = 0; i < 100; i++){
+            int pricecode = (int)(Math.random()*3);
+            int days_rented = (int)(Math.random()*100);
+            Movie movie = new Movie(title,pricecode);
+            Rental rental = new Rental(movie, days_rented);
+
+            assertEquals(calcMoney(pricecode,days_rented),rental.getMoney(),0.0);
+
+        }
+    }
+
+    public double calcMoney(int pricecode, int days_rented) {
+        double result = 0.0;
+        switch (pricecode) {
+            case 0:
+                result += 2;
+                if (days_rented > 2)
+                    result += (days_rented - 2) * 1.5;
+                break;
+            case 1:
+                result += days_rented * 3;
+                break;
+            case 2:
+                result += 1.5;
+                if (days_rented > 3)
+                    result += (days_rented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+
 }
